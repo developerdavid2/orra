319622b2510d32427ef4e096992d3d77065f144c
Jacobs David <jacobsdavid.dr@gmail.com>
2026-08-25 13:30:34 +0100

    remove experimental field from nextjs config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,13 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  experimental: {
-    staleTimes: { dynamic: 120 },
-  },
+
   images: {
     remotePatterns: [{ protocol: "https", hostname: "eqr61bekec.ufs.sh" }],
   },
-  generateBuildId: async () => `build-${Date.now()}`,
 };
 
 export default nextConfig;
